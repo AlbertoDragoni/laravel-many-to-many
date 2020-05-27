c036730b5b44c6e29c0bcf131069e2facf426c21
Alberto Dragoni <albertodragoni@yahoo.it>
2020-05-27 17:58:25 +0200

    creazione primi modelli

--- app/User.php
+++ app/User.php
@@ -36,4 +36,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function info(){
+      return $this->hasOne('App\InfoUser');
+    }
+    public function categories(){
+      return $this->hasMany('App\Category');
+    }
 }
