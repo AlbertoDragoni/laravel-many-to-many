194a5fba49e1b12ed27b6c8cad1c36fe9850c7f8
Alberto Dragoni <albertodragoni@yahoo.it>
2020-05-28 17:54:56 +0200

    page controller ok

--- app/Http/Controllers/Admin/PageController.php
+++ app/Http/Controllers/Admin/PageController.php
@@ -6,13 +6,13 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+
 use App\User;
 use App\Page;
 use App\Category;
 use App\Tag;
 use App\Photo;
-
-
 
 class PageController extends Controller
 {
@@ -23,7 +23,9 @@ class PageController extends Controller
      */
     public function index()
     {
-        //
+        $pages = Page::paginate();
+
+        return view('admin.pages.index', compact('pages'));
     }
 
     /**
@@ -33,10 +35,10 @@ class PageController extends Controller
      */
     public function create()
     {
-      $categories = Category::all();
-      $tags = Tag::all();
-      $photos = Photo::all();
-      return view('admin.pages.create', compact('categories', 'tags', 'photos'));
+        $categories = Category::all();
+        $tags = Tag::all();
+        $photos = Photo::all();
+        return view('admin.pages.create', compact('categories', 'tags', 'photos'));
     }
 
     /**
@@ -47,22 +49,39 @@ class PageController extends Controller
      */
     public function store(Request $request)
     {
-      $validator = Validator::make($request->all(), [
+        $data = $request->all();
+        //dd($request->all());
+        //validazione
+        $validator = Validator::make($data, [
             'title' => 'required|max:200',
             'body' => 'required',
-            'category_id'=> 'required|exists:categories,id',
-            'tags'=> 'required|array',
-            'photos'=> 'required|array',
-            'tags.*'=> 'exists:tags,id',
-            'photos.*'=> 'exists:photos,id',
-
+            'category_id' => 'required|exists:categories,id',
+            'tags' => 'required|array',
+            'photos' => 'required|array',
+            'tags.*' => 'exists:tags,id',
+            'photos.*' => 'exists:photos,id'
         ]);
 
         if ($validator->fails()) {
             return redirect()->route('admin.pages.create')
-                        ->withErrors($validator)
-                        ->withInput();
+            ->withErrors($validator)
+            ->withInput();
         }
+
+        $page = new Page;
+        $data['slug'] = $data['slug'] = Str::slug($data['title'] , '-');
+        $data['user_id'] = Auth::id();
+        $page->fill($data);
+        $saved = $page->save();
+
+        if(!$saved) {
+            dd('error');
+        }
+
+        $page->tags()->attach($data['tags']);
+        $page->photos()->attach($data['photos']);
+
+        return redirect()->route('admin.pages.show', $page->id);
     }
 
     /**
@@ -73,7 +92,9 @@ class PageController extends Controller
      */
     public function show($id)
     {
-        //
+        $page = Page::findOrFail($id);
+
+        return view('admin.pages.show', compact('page'));
     }
 
     /**
@@ -84,7 +105,12 @@ class PageController extends Controller
      */
     public function edit($id)
     {
-        //
+        $page = Page::findOrFail($id);
+        $categories = Category::all();
+        $tags = Tag::all();
+        $photos = Photo::all();
+
+        return view('admin.pages.edit', compact('page','categories', 'tags', 'photos'));
     }
 
     /**
@@ -96,7 +122,33 @@ class PageController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $page = Page::findOrFail($id);
+        $data = $request->all();
+        $userId = Auth::id();
+        $author =$page->user_id;
+
+        if($userId != $author) {
+            abort('404');
+        }
+
+        //validation
+
+        $page->fill($data);
+        $updated = $page->update();
+        if (!$updated) {
+           return redirect()->back();
+        }
+
+        //manualmente elimino tutti i collegamenti nella tabella ponte
+        //$page->tags()->detach();
+        //ricreo i record in tabella ponte
+        //$page->tags()->attach($data['tags']);
+
+        //sincronizza tabella ponte
+        $page->tags()->sync($data['tags']);
+        $page->photos()->sync($data['photos']);
+
+        return redirect()->route('admin.pages.show', $page->id);
     }
 
     /**
@@ -107,6 +159,16 @@ class PageController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $page = Page::findOrFail($id);
+
+        $page->tags()->detach();
+        $page->photos()->detach();
+        $deleted = $page->delete();
+
+        if(!$deleted){
+            return redirect()->back();
+        }
+
+        return redirect()->route('admin.pages.index');
     }
 }
